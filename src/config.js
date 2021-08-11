7d25f09b66e5e1e7fec9117bd72f6c38070ade59
kccrtv <kimcincocreative@gmail.com>
2021-08-11 13:19:48 -0500

    update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const APIurl =
 	window.location.hostname === 'localhost'
 		? 'http://localhost:3111'
-		: 'http://b99api.com';
+		: 'https://quizzical-bose-ab326c.netlify.app';
 
 export default APIurl;
