4f459640ec74b1f247f7f06f9fd7d31f6793f3da
kccrtv <kimcincocreative@gmail.com>
2021-08-11 15:46:51 -0500

    update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const APIurl =
 	window.location.hostname === 'localhost'
 		? 'http://localhost:3111'
-		: 'https://employee-directory-backend.netlify.app';
+		: 'https://employee-directory-backend.herokuapp.com';
 
 export default APIurl;
